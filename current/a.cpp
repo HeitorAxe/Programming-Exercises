c5f39fa9d0267319486f222052eacf09e3e8b318
heitor <heitorotieh8@gmail.com>
2024-05-12 02:00:57 -0300

    :sparkles: bee_1934

--- current/a.cpp
+++ current/a.cpp
@@ -1,39 +1,58 @@
 #include <bits/stdc++.h>
-
+#define DBG(x) cout << "[" << #x << "]: " << x << endl
+#define F(x) std::fixed << std::setprecision(1) << (x)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#include <bits/stdc++.h>
 using namespace std;
+//1966
+long knapSack(long  W, vector<long> wt, vector<long> val, long  n)
+{
+    long dp[W + 1];
+    memset(dp, 0, sizeof(dp));
+
+    for (long i = 1; i < n + 1; i++) {
+        for (long w = W; w >= 0; w--) {
+            if (wt[i - 1] <= w)
+                dp[w] = max(dp[w],
+                            dp[w - wt[i - 1]] + val[i - 1]);
+        }
+    }
+    return dp[W];
+}
 
 int main() {
 
-    int l, c, auxi;
-    char auxc;
+    long  n, b, aux;
+    vector<long> vol;
+    vector<long> price;
 
-    map<char, int> x;
-    vector<vector<char>> ml;
-    vector<vector<char>> mc;
-    vector<int> vl;
-    vector<int> vc;
+    cin>>n>>b;
 
-    cin>>l>>c;
-
-    for(int i = 0; i<l; i++){
-        if(i=l-1){
-            for(int j = 0; j<c; j++){
-                if(j != c-1){
-                    cin>>auxi;
-                    ml[i][j] = auxi;
-                }else{
-                    cin>>auxc;
-                    vl[i] = auxc;
-                }
-            }
-        }else{
-            for(int j = 0; j<c; j++){
-                cin>>auxi;
-                vc.push_back(auxi);
-            }
-        }
+    for(long  i = 0; i<n; i++){
+        cin>>aux;
+        vol.push_back(aux);
     }
 
+    for(long  i = 0; i<n; i++){
+        cin>>aux;
+        price.push_back(aux);
+    }
+    long  totalVolume = knapSack(b, price, vol, n);
+    
+    long count = 0;
+    while(totalVolume>count){
+        count++;
+        totalVolume -= count;
+    }
+
+    cout << count <<endl;
 
     return 0;
 }
