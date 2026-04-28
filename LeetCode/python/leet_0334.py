e4fbf2bc2965eeb31ddc79335d00fe53b5cfbef9
HeitorAxe <heitorotieh8@gmail.com>
2026-04-27 21:48:13 -0300

    :sparkles: leet_0334 py

--- LeetCode/python/leet_0334.py
+++ LeetCode/python/leet_0334.py
@@ -1,6 +1,24 @@
 from typing import List
 
-
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
+        ma = max(nums)
+        mi = min(nums)
+        mil = []
+        mal = []
+        count = 0
+        for num in nums:
+            mal.append(ma)
+            ma = min(ma, num)
+        for num in nums[::-1]:
+            mil.append(mi)
+            mi = max(mi, num)
+        mil = mil[::-1]
+        print(mal)
+        print(mil)
+        for i in range(len(nums)):
+            if nums[i] > mal[i] and nums[i] < mil[i]:
+                return True
+        return False
+
         
